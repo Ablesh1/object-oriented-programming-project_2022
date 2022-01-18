74c19760f7569521a64dff24c9a2d2ce56fb9592
Ablesh1 <michal.ablewski19@gmail.com>
2022-01-18 21:43:18 +0100

    Change deposit and withdraw logic

--- src/main/java/com/example/po/NPCAccount.java
+++ src/main/java/com/example/po/NPCAccount.java
@@ -6,45 +6,57 @@ public class NPCAccount {
     NPC owner;
     BankBackend bankBackend;
 
-    //Account tracks how indebt or rich is NPC
-    private double deposit;
-    private double credit;
+    //Account tracks how in debt or rich is NPC
+    private Double accountMoney;
+    private Double deposit;
+    private Double credit;
 
     //Balance will be handy when doing AI
     private double balance;
 
-    public NPCAccount(NPC npc, double deposit, BankBackend bankBackendPass){
+    public NPCAccount(NPC npc, Double accountMoney, BankBackend bankBackendPass){
         owner = npc;
-        this.deposit = deposit;
-        this.credit = 0;
+        this.accountMoney = accountMoney;
+        this.deposit = 0.0;
+        this.credit = 0.0;
         bankBackend = bankBackendPass;
     }
 
-    //Add withdrawFromAccount
-    public void depositOnAccount(double toDeposit){
+    public void depositOnAccount(Double toDeposit){
         if (owner.getPersonBelongings() >= toDeposit){
-        this.deposit += toDeposit;
-        owner.setPersonBelongings(toDeposit);
+        this.accountMoney += toDeposit;
+        owner.setPersonBelongings(owner.getPersonBelongings() - toDeposit);
         }
     }
 
-    public double checkCredit(){
-        if (this.deposit < 0) {
-            this.credit += this.deposit;
-            this.deposit = 0;
+    public void  withdrawFromAccount(Double toWithdraw) {
+        if (this.accountMoney - toWithdraw >= 0.0) {
+            this.accountMoney -= toWithdraw;
+            owner.setPersonBelongings(owner.getPersonBelongings() + toWithdraw);
+        }
+    }
+
+    public Double checkCredit(){
+        if (this.accountMoney < 0) {
+            this.credit += this.accountMoney;
+            this.accountMoney = 0.0;
         }
 
-        if (this.deposit > 0 && this.credit > 0){
-            this.balance = this.deposit - this.credit;
+        if (this.accountMoney > 0 && this.credit > 0){
+            this.balance = this.accountMoney - this.credit;
             if (this.balance >= 0){
-                this.deposit = this.balance;
-                this.credit = 0;
+                this.accountMoney = this.balance;
+                this.credit = 0.0;
             }
         }
         return this.credit;
     }
 
-    public double currentMoney(){
-        return deposit;
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public Double getAccountMoney() {
+        return accountMoney;
     }
+
 }
