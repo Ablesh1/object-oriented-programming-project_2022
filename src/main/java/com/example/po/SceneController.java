e471165edfac0f1cd5772a706c6e82050a5517a4
Ablesh1 <michal.ablewski19@gmail.com>
2022-01-19 03:06:15 +0100

    Adjust SceneController to new mechanics - plenty of Loan and Investment methods added

--- src/main/java/com/example/po/SceneController.java
+++ src/main/java/com/example/po/SceneController.java
@@ -20,6 +20,7 @@ public class SceneController{
     TextArea CurrencyArea;
     @FXML
     TextArea StockArea;
+
     @FXML
     TextField ATMArea;
     @FXML
@@ -30,6 +31,30 @@ public class SceneController{
     Button DepositButton;
     @FXML
     Button WithdrawButton;
+
+    @FXML
+    TextField LoanArea;
+    @FXML
+    TextField InstallmentNumber;
+    @FXML
+    TextField LoanLeft;
+    @FXML
+    TextField InstallmentAmount;
+    @FXML
+    Button LoanButton;
+    @FXML
+    Button PayButton;
+
+    @FXML
+    TextField InvestmentArea;
+    @FXML
+    TextField InvestmentDuration;
+    @FXML
+    TextField InvestmentAmount;
+    @FXML
+    Button InvestmentButton;
+    @FXML
+    Button CloseInvestButton;
 
     //private CurrencyRateDepBack currencyRate;
     //private StockRateDepBack stockRate;
@@ -96,6 +121,30 @@ public class SceneController{
         stage.show();
     }
 
+    public void switchToATM(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("ATM.fxml"));
+        stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToLoan(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("BankLoan.fxml"));
+        stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToInvestment(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("BankInvestment.fxml"));
+        stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public void switchToTransfers(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("Transfers.fxml"));
         stage = (Stage)((Node) event.getSource()).getScene().getWindow();
@@ -120,6 +169,13 @@ public class SceneController{
         stage.show();
     }
 
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public void OnClicked(ActionEvent event) throws IOException {
+        System.out.println("Something happened");
+    }
+
     public void updateCurrentMoney(){
         NPC player = bankBackend.getClient(1);
         CurrentMoneyArea.setText(String.valueOf(player.getAccountMoneyAI()));
@@ -129,6 +185,24 @@ public class SceneController{
         NPC player = bankBackend.getClient(1);
         BelongingsArea.setText(String.valueOf(player.getPersonBelongings()));
     }
+
+    public void updateInstallmentAmount() {
+        NPC player = bankBackend.getClient(1);
+        InstallmentAmount.setText(String.valueOf(player.getInstallmentAmount()));
+    }
+
+    public void updateActualDebt() {
+        NPC player = bankBackend.getClient(1);
+        LoanLeft.setText(String.valueOf(player.getActualDebt()));
+    }
+
+    public void updateInvestment() {
+        NPC player = bankBackend.getClient(1);
+        InvestmentAmount.setText(String.valueOf(player.getBankInvestment()));
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     public void deposit(ActionEvent event) throws IOException{
         NPC player = bankBackend.getClient(1);
@@ -154,17 +228,58 @@ public class SceneController{
         updatePersonBelongings();
     }
 
-    public void toATM(ActionEvent event) throws IOException{
-        Parent root = FXMLLoader.load(getClass().getResource("ATM.fxml"));
-        stage = (Stage)((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+    public void loan(ActionEvent event) throws IOException{
+        NPC player = bankBackend.getClient(1);
+        try{
+            player.takeLoan(Double.valueOf(LoanArea.getText()), Integer.valueOf(InstallmentNumber.getText()));
+        }
+        catch(NumberFormatException e){
+            ;
+        }
+        updateCurrentMoney();
+        updateInstallmentAmount();
+        updateActualDebt();
     }
 
-    public void OnClicked(ActionEvent event) throws IOException {
-        System.out.println("Something happened");
+    public void pay(ActionEvent event) throws IOException {
+        NPC player = bankBackend.getClient(1);
+        try{
+            player.payLoan();
+        }
+        catch(NumberFormatException e){
+            ;
+        }
+        updateCurrentMoney();
+        updateInstallmentAmount();
+        updateActualDebt();
     }
+
+    public void investment(ActionEvent event) throws IOException{
+        NPC player = bankBackend.getClient(1);
+        try{
+            player.makeInvestment(Double.valueOf(InvestmentArea.getText()), Integer.valueOf(InvestmentDuration.getText()));
+        }
+        catch(NumberFormatException e){
+            ;
+        }
+        updateCurrentMoney();
+        updateInvestment();
+    }
+
+    public void close(ActionEvent event) throws IOException{
+        NPC player = bankBackend.getClient(1);
+        try{
+            player.closeInvestment();
+        }
+        catch(NumberFormatException e){
+            ;
+        }
+        updateCurrentMoney();
+        updateInvestment();
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     //Wyświetlanie obecnej giełdy spółek i walut
     public void ShowCurrency() throws IOException{
