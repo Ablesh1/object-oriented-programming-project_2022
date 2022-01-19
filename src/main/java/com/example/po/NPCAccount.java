953698d9abfa11c85d22e8e5cef8c2137eaa5a2e
Ablesh1 <michal.ablewski19@gmail.com>
2022-01-19 03:01:22 +0100

    Add Loan and Investment mechanics

--- src/main/java/com/example/po/NPCAccount.java
+++ src/main/java/com/example/po/NPCAccount.java
@@ -8,19 +8,87 @@ public class NPCAccount {
 
     //Account tracks how in debt or rich is NPC
     private Double accountMoney;
-    private Double deposit;
     private Double credit;
 
     //Balance will be handy when doing AI
     private double balance;
 
-    public NPCAccount(NPC npc, Double accountMoney, BankBackend bankBackendPass){
+    private Double bankLoan;
+    private Integer installmentNumber;
+    private Double installmentAmount;
+    private Double bankInvestment;
+    private Integer investmentDuration;
+
+    private int counter = 0;
+    private boolean trust = true;
+    private double actualDebt = 0;
+    private double interestRate = 0.12;
+
+
+    public NPCAccount(NPC npc, Double accountMoney, Double bankLoan, Integer installmentNumber, Double bankInvestment, Integer investmentDuration, BankBackend bankBackendPass){
         owner = npc;
         this.accountMoney = accountMoney;
-        this.deposit = 0.0;
-        this.credit = 0.0;
+        this.bankLoan = bankLoan;
+        this.installmentNumber = installmentNumber;
+        this.bankInvestment = bankInvestment;
+        this.investmentDuration = investmentDuration;
         bankBackend = bankBackendPass;
+        this.credit = 0.0;
     }
+
+    public Double getAccountMoney() {
+        return accountMoney;
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public Double getBankInvestment() {
+        return bankInvestment;
+    }
+
+    public Integer getInvestmentDuration() {
+        return investmentDuration;
+    }
+
+    public void setBankInvestment(Double bankInvestment) {
+        this.bankInvestment = bankInvestment;
+    }
+
+    public void setInvestmentDuration(Integer investmentDuration) {
+        this.investmentDuration = investmentDuration;
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public Double getInstallmentAmount() {
+
+        if(this.installmentNumber > 0 && this.bankLoan > 0) {
+
+            double q = 1 + (this.interestRate/12);
+            double L = Math.pow(q, this.installmentNumber) * (q - 1);
+            double M = Math.pow(q, this.installmentNumber) - 1;
+
+            this.installmentAmount = this.bankLoan * L / M;
+        }
+        return installmentAmount;
+    }
+
+    public double getActualDebt() {
+        return actualDebt;
+    }
+
+    public void setBankLoan(Double bankLoan) {
+        this.bankLoan = bankLoan;
+    }
+
+    public void setInstallmentNumber(Integer installmentNumber) {
+        this.installmentNumber = installmentNumber;
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     public void depositOnAccount(Double toDeposit){
         if (owner.getPersonBelongings() >= toDeposit){
@@ -36,6 +104,47 @@ public class NPCAccount {
         }
     }
 
+    public void takeBankLoan(Double bankLoan, Integer installmentNumber) {
+        if(this.trust) {
+            setBankLoan(bankLoan);
+            setInstallmentNumber(installmentNumber);
+            this.accountMoney += this.bankLoan;
+            this.actualDebt = getInstallmentAmount() * this.installmentNumber;
+            this.trust = false;
+        }
+    }
+
+    public void payInstallment() {
+        if (this.accountMoney > 0 &&this.actualDebt > 0 && this.counter < this.installmentNumber) {
+            this.accountMoney -= getInstallmentAmount();
+            this.actualDebt -= getInstallmentAmount();
+            this.counter ++;
+            if (this.actualDebt < 0.01) {
+                this.actualDebt = 0;
+            }
+        }
+        else {
+            this.actualDebt = 0;
+            this.counter = 0;
+            this.trust = true;
+        }
+    }
+
+    public void makeBankInvestment(Double bankInvestment, Integer investmentDuration) {
+        if(this.accountMoney - bankInvestment >= 0) {
+            this.accountMoney -= bankInvestment;
+            this.bankInvestment += bankInvestment;
+        }
+    }
+
+    public void closeInvestment() {
+        this.accountMoney += bankInvestment;
+        this.bankInvestment =  0.0;
+    }
+
+
+
+    //Mogę to usunąć?
     public Double checkCredit(){
         if (this.accountMoney < 0) {
             this.credit += this.accountMoney;
@@ -51,12 +160,4 @@ public class NPCAccount {
         }
         return this.credit;
     }
-
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    public Double getAccountMoney() {
-        return accountMoney;
-    }
-
 }
